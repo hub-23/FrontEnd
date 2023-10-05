29411442fe9fa004cfc850e801f18a468159ed64
mykola dychkevych <mykola.dychkevych@sigma.software>
2023-10-05 08:40:10 +0300

    remove extra packages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,9 +3,13 @@ import { Routes, Route } from 'react-router';
 
 import Home from '../../pages/home/homePage';
 
+import GeneralStyles from '../../generalStyles';
+
 const App = () => {
   return (
     <>
+      <GeneralStyles/>
+
       <Routes>
         <Route exact path='/' element={ <Home/> }/>
       </Routes>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app/App';
 
-import './index.scss';
-
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
     <React.StrictMode>
