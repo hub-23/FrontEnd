4736f3b79dfe7272e6d22c48b0a78019f471629e
uliabileckaya <uliabiletskaya@gmail.com>
2023-10-23 16:58:00 +0200

    Header - Init

---
+++ src/components/Header/Select/Language/Language.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import styles from './language.module.css'
+const Language = () => {
+
+    const [active, setActive] = useState(true)
+
+
+    return (
+        <div className={styles.language}>
+            <p>UA</p>
+            <div className={styles.divider}></div>
+            <p>EN</p>
+        </div>
+    );
+};
+
+export default Language;
