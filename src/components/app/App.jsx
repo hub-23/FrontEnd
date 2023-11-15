0250d8a965962d84d4fcfa35e5c5b38cfc703b78
L-Marina <marina.liemak@gmail.com>
2023-11-15 12:08:14 +0200

    rename home again

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
 import Theme from '../../theme/Theme';
-import Home from '../../pages/HomePage';
+import Home from '../../pages/Home';
 
 import GeneralStyles from '../../generalStyles';
 
