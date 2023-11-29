6757220eab22378893bc3a1092018956c3d4bd22
K-Basket <koshykkv@gmail.com>
2023-11-29 13:34:01 +0200

    added Homepage Block 2 Popular classes

--- src/components/PopularСlasses/PopularСlasses.jsx
+++ src/components/PopularСlasses/PopularСlasses.jsx
@@ -1,53 +1,69 @@
 import {
-  Button,
-  Container,
-  SeeMore,
-  Title,
-  TopLessons,
+    Button,
+    Container,
+    SeeMore,
+    Title,
+    TopLessons,
 } from './PopularСlasses.styled';
 import sprite from './sprite.svg';
-import { dataTopLessons as data } from './backend_temp';
+import { dataTopLessons } from './backend_temp';
+import { useEffect, useState } from 'react';
 
 export const PopularСlasses = () => {
-  return (
-    <Container>
-      <Title>Популярні заняття</Title>
+    const [data, setData] = useState([]);
+    const [isSeeMore, setIsSeeMore] = useState(true);
 
-      <TopLessons>
-        {data.map(({ nameLesson, numTeach }) => (
-          <li>
-            <h2>{nameLesson}</h2>
+    useEffect(() => {
+        if (isSeeMore) {
+            let res = [];
+            for (let i = 0; i < 18; i += 1) {
+                res.push(dataTopLessons[i]);
+            }
+            return setData(res);
+        }
 
-            <div>
-              <p>{numTeach} викладач</p>
+        setData(dataTopLessons);
+    }, [isSeeMore]);
 
-              <Button
-                onClick={() => {
-                  console.log('click on Item lesson');
-                }}
-              >
-                <svg width="24px" height="24px">
-                  <use href={`${sprite}#icon-arrow`}></use>
-                </svg>
-              </Button>
-            </div>
-          </li>
-        ))}
-      </TopLessons>
+    const addSeeMore = () => {
+        setIsSeeMore((prev) => !prev);
+    };
 
-      <SeeMore>
-        <p>Дивитися більше</p>
+    return (
+        <Container>
+            <Title>Популярні заняття</Title>
 
-        <Button
-          onClick={() => {
-            console.log('click on Дивитися більше');
-          }}
-        >
-          <svg width="24px" height="24px">
-            <use href={`${sprite}#icon-arow_down`}></use>
-          </svg>
-        </Button>
-      </SeeMore>
-    </Container>
-  );
+            <TopLessons>
+                {data.map(({ id, nameLesson, numTeach }) => (
+                    <li key={id}>
+                        <h2>{nameLesson}</h2>
+
+                        <div>
+                            <p>{numTeach} викладач</p>
+
+                            <Button
+                                onClick={() => {
+                                    console.log('click on Item lesson');
+                                }}
+                            >
+                                <svg width="24px" height="24px">
+                                    <use href={`${sprite}#icon-arrow`}></use>
+                                </svg>
+                            </Button>
+                        </div>
+                    </li>
+                ))}
+            </TopLessons>
+
+            <SeeMore>
+                {isSeeMore ? <p>Дивитися більше</p> : <p>Дивитися меньше</p>}
+
+                <Button $isActive={isSeeMore} onClick={addSeeMore}>
+                    <svg width="24px" height="24px">
+                        <use href={`${sprite}#icon-arow_down`}></use>
+                    </svg>
+                </Button>
+            </SeeMore>
+        </Container>
+    );
 };
