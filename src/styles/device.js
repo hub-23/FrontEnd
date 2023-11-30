3905d1910d6a3899ba7bbd1b9eee4911f4be256e
L-Marina <marina.liemak@gmail.com>
2023-11-30 13:37:08 +0200

    add screen extension variables

---
+++ src/styles/device.js
@@ -0,0 +1,19 @@
+const size = {
+  xs: 0,
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  xxl: '1440px',
+  xxxl: '2560',
+};
+
+export const device = {
+  xs: `(max-width: ${size.xs})`,
+  sm: `(max-width: ${size.sm})`,
+  md: `(max-width: ${size.md})`,
+  lg: `(max-width: ${size.lg})`,
+  xl: `(max-width: ${size.xl})`,
+  xxl: `(max-width: ${size.xxl})`,
+  xxxl: `(max-width: ${size.xxl})`,
+};
