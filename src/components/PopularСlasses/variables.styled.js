b1bb3c879eff9f8f2112ea49146d0c2e48df221f
K-Basket <koshykkv@gmail.com>
2023-11-30 18:58:28 +0200

    update variables.styled.js

--- src/components/PopularСlasses/variables.styled.js
+++ src/components/PopularСlasses/variables.styled.js
@@ -2,9 +2,9 @@ export const white = '#FFFFFF';
 export const black = '#2d2d2d';
 export const accent = '#E3669C';
 export const primary = '#113268';
+
 export const device = {
   mobile: '(min-width: 360px)',
-
   desktop: '(min-width: 1440px)',
 };
 
