797b44b3020936691ee161124cfa95a6ad912efb
OIgaB <linkbolya@gmail.com>
2024-01-04 23:57:03 +0200

    style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as S from './Footer.styled';
-import logo from '../../assets/home/logo.svg';
-// import sprite from '../../assets/sprite.svg';
 import { IconSvg } from '../common/IconSvg';
+
 
 export const Footer = () => {
   return (
@@ -11,43 +10,49 @@ export const Footer = () => {
       <S.FooterContainer>
         <S.ContentOrganizer>
           <Link to='/'>
-            <img src={ logo } alt='HUB23'/>
+            <S.Logo />
           </Link>
-          <div>
+          <S.NavWrapper>
             <S.Header>Інформація</S.Header>
-            <Link to='/teacher'>Викладачі</Link>
-            <Link to='/about'>Про нас</Link>
-            <Link to='/feedback'>Відгуки</Link>
-          </div>
+            <ul>
+              <S.NavItem><S.NavLink to='/teacher'>Викладачі</S.NavLink></S.NavItem>
+              <S.NavItem><S.NavLink to='/about'>Про нас</S.NavLink></S.NavItem>
+              <S.NavItem><S.NavLink to='/feedback'>Відгуки</S.NavLink></S.NavItem>
+            </ul>
+          </S.NavWrapper>
           <div>
-            <S.Header>Контакти</S.Header>
             <S.ContactsWrapper>
-              <IconSvg xlWidth='24px' xlHeight='24px' icon='icon-clock' />
-              <S.Contacts>Пн-Нд // 9:00 - 20:00</S.Contacts>
+              <S.Header>Контакти</S.Header>
+              <S.ContactsItem>
+                <IconSvg xlWidth='24px' xlHeight='24px' icon='icon-clock' />
+                <S.Contacts>Пн-Нд // 9:00 - 20:00</S.Contacts>
+              </S.ContactsItem>
+              <S.ContactsItem>
+                <IconSvg xlWidth='24px' xlHeight='24px' icon='icon-envelope' />
+                <S.Email href="mailto:hub23project@gmail.com">hub23project@gmail.com</S.Email>
+              </S.ContactsItem>
             </S.ContactsWrapper>
-            <S.ContactsWrapper>
-              <IconSvg xlWidth='24px' xlHeight='24px' icon='icon-envelope' />
-              <S.Contacts>hub23project@gmail.com</S.Contacts>
-            </S.ContactsWrapper>
+
             <S.Header>Слідкуй за нами</S.Header>
-            <div>
-              <IconSvg xlWidth='24px' xlHeight='24px' icon='icon-youtube' />
-              <IconSvg xlWidth='24px' xlHeight='24px' icon='icon-facebook' />
-            </div>
+            <S.SocList>
+              <li><IconSvg xlWidth='24px' xlHeight='24px' icon='icon-youtube' /></li>
+              <li><IconSvg xlWidth='24px' xlHeight='24px' icon='icon-facebook' /></li>
+            </S.SocList>
           </div>
+
           <div>
             <S.Header>Залишились питання?</S.Header>
-            <button>Заповнити форму</button>
+            <S.FormBtn>Заповнити форму</S.FormBtn>
           </div>
         </S.ContentOrganizer>
-        <div>
+        <S.TermsRightsWrapper>
           <div>
             <S.Terms href="">Політика конфіденційності</S.Terms>
             <span>|</span>
             <S.Terms href="">Умови використання</S.Terms>
           </div>
           <S.Rights>Усі права захищені 2023</S.Rights>
-        </div>
+        </S.TermsRightsWrapper>
       </S.FooterContainer>
     </S.FooterSection>
   );
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import { ReactComponent as LogoSvg } from '../../assets/home/logo.svg';
+import { Link } from 'react-router-dom';
 
-export const FooterSection = styled.section`
+export const FooterSection = styled.footer`
     padding-top: 64px;
     padding-bottom: 22px;
     background-color:  ${( props ) => props.color || props.theme.colors.primary};
@@ -18,36 +20,106 @@ export const ContentOrganizer = styled.div`
     display: flex;
 `;
 
+export const Logo = styled( LogoSvg )`
+  margin-right: 127px;
+  path.text-path {
+    fill: #fff;
+  }
+`;
+
+export const NavWrapper = styled.div`
+  margin-right: 97px;
+`;
+
 export const Header = styled.h4`
-    font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};;
+    margin-bottom: 16px;
+    font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
     font-size: 20px;
     font-weight: 500;
-    line-height: 140%; /* 28px */
+    line-height: 1.4;
     letter-spacing: 0.2px;
     color: ${( props ) => props.color || props.theme.colors.white};
 `;
 
+export const NavItem = styled.li`
+    &:not(:last-child) {
+       margin-bottom: 12px;  
+    }     
+`;
+
+export const NavLink = styled( Link )`
+    font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 1.4;
+    letter-spacing: 0.2px;
+    color: ${( props ) => props.color || props.theme.colors.white};     
+`;
+
 export const ContactsWrapper = styled.div`
+    margin-right: 145px;
+    margin-bottom: 32px;
+`;
+
+export const ContactsItem = styled.div`
     display: flex;
     gap: 8px;
-    margin-bottom: 32px;
-    /* align-items: center; */
+    &:not(:last-child) {
+      margin-bottom: 12px;
+    }
 `;
 
 export const Contacts = styled.p`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
     font-size: 20px;
     font-weight: 400;
-    line-height: 140%; /* 28px */
+    line-height: 1.4;
     letter-spacing: 0.2px;
     color: ${( props ) => props.color || props.theme.colors.white};
+`;
+
+export const Email = styled.a`
+    font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 1.4;
+    letter-spacing: 0.2px;
+    color: ${( props ) => props.color || props.theme.colors.white};
+`;
+
+export const SocList = styled.ul`
+    display: flex;
+    gap: 12px;
+    margin-right: 145px;
+`;
+
+export const FormBtn = styled.button`
+    width: 224px;
+    border: none;
+    border-radius: 16px 0;
+    padding: 14px 24px;
+    font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1.4;
+    letter-spacing: 0.2px;
+    color: ${( props ) => props.color || props.theme.colors.black};
+`;
+
+export const TermsRightsWrapper = styled.div`
+    margin-top: 125px;
+    text-align: center;
+    & > div {
+        width: 390px;
+        margin: 0 auto 12px auto;
+    }
 `;
 
 export const Terms = styled.a`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
     font-size: 16px;
     font-weight: 400;
-    line-height: 140%; /* 22.4px */
+    line-height: 1.4;
     letter-spacing: 0.16px;
     color: ${( props ) => props.color || props.theme.colors.white};
 `;
@@ -56,7 +128,8 @@ export const Rights = styled.p`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
     font-size: 16px;
     font-weight: 300;
-    line-height: 140%; /* 22.4px */
+    line-height: 1.4;
     letter-spacing: 0.16px;
-    color: ${( props ) => props.color || props.theme.colors.white};
+    color: ${( props ) => props.color || props.theme.colors.white}; 
+    
 `;
