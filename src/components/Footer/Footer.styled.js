feb5c143fb9e1e0ff3b4e05c575971af1883ec7f
OIgaB <linkbolya@gmail.com>
2024-01-06 23:52:53 +0200

    apply media-rules to footer

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,73 +1,93 @@
 import styled, { css } from 'styled-components';
-import { device } from '../../styles/device';
+// import { device } from '../../styles/device';
 import { transition } from '../../utils/variables.styled';
 import { ReactComponent as LogoSvg } from '../../assets/home/logo.svg';
 import { Link } from 'react-router-dom';
 
 export const FooterSection = styled.footer`
-    padding-top: 64px;
-    padding-bottom: 22px;
     background-color:  ${( props ) => props.color || props.theme.colors.primary};
-    @media ${device.sm} {
-        padding-top: 40px;
-        padding-bottom: 20px;
+    padding-top: 40px;
+    padding-bottom: 20px;
+    @media screen and (min-width: 360px) {
+        padding-top: 64px;
+        padding-bottom: 22px;
     }
 `;
 
 export const FooterContainer = styled.div`
     border: 1px solid yellow;
-    max-width: 1440px;
+    width: 100%;
     margin: 0 auto;
-    padding-left: 160px;
-    padding-right: 158px;
-    @media ${device.sm} {
-        background-color: darkblue;
-        max-width: 576px;
-        padding-left: 20px;
-        padding-right: 20px;
+    padding-left: 20px;
+    padding-right: 20px;    
+    @media screen and (min-width: 360px) {
+        background-color: goldenrod;
+        width: 360px;
+    }
+    @media screen and (min-width: 576px) {
+        background-color: firebrick;
+        width: 576px;
+    }
+    @media screen and (min-width: 768px) {
+        background-color: blueviolet;
+        width: 768px;
+        padding-left: 160px;
+        padding-right: 158px;
+    }
+    @media screen and (min-width: 992px) {
+        background-color: red;
+        width: 992px;
+    }
+    @media screen and (min-width: 1200px) {
+        background-color: blue;
+        width: 1200px;
+    }
+    @media screen and (min-width: 1440px) {
+        background-color: yellowgreen;
+        width: 1440px;
     }
 `;
 
 export const ContentOrganizer = styled.div`
-    display: flex;
-    @media ${device.sm} {
-        display: block;
+    display: block;
+    @media screen and (min-width: 360px) {
+        display: flex;
     }
 `;
 
 export const Logo = styled( LogoSvg )`
-  margin-right: 127px;
+    width: 93px;
+    height: 24px;
+    margin-right: 0;
+    margin-bottom: 40px;
   path.text-path {
     fill: #fff;
   }
-  @media ${device.sm} {
-    margin-right: 0;
-    margin-bottom: 40px;
-    width: 93px;
-    height: 24px;
+  @media screen and (min-width: 360px) {
+    margin-right: 127px;
   }
 `;
 
 export const NavWrapper = styled.div`
-  margin-right: 97px;
-  @media ${device.sm} {
-    margin-right: 0;
-    margin-bottom: 40px;
+  margin-right: 0;
+  margin-bottom: 40px;
+  @media screen and (min-width: 360px) {
+    margin-right: 97px;
   }
 `;
 
 export const Header = styled.h4`
     margin-bottom: 16px;
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 20px;
+    font-size: 16px;
     font-weight: 500;
     line-height: 1.4;
-    letter-spacing: 0.2px;
+    letter-spacing: 0.16px;
     text-align: left;
     color: ${( props ) => props.color || props.theme.colors.white};
-    @media ${device.sm} {
-      font-size: 16px;
-      letter-spacing: 0.16px;
+    @media screen and (min-width: 360px) {
+      font-size: 20px;
+      letter-spacing: 0.2px;
     }
 `;
 
@@ -79,23 +99,23 @@ export const NavItem = styled.li`
 
 export const NavLink = styled( Link )`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 20px;
+    font-size: 14px;
     font-weight: 400;
     line-height: 1.4;
     letter-spacing: 0.2px;
     color: ${( props ) => props.color || props.theme.colors.white};     
-    @media ${device.sm} {
-        font-size: 14px;
+    @media screen and (min-width: 360px) {
+        font-size: 20px;
     }
 `;
 
 export const ContactsWrapper = styled.div`
-    margin-right: 145px;
-    margin-bottom: 32px;
-    @media ${device.sm} {
-        font-size: 14px;
-        margin-right: 0;
-        margin-bottom: 40px;
+    margin-right: 0;
+    margin-bottom: 40px;
+    font-size: 14px;
+    @media screen and (min-width: 360px) {
+        margin-right: 145px;
+        margin-bottom: 32px;
     }
 `;
 
@@ -109,13 +129,13 @@ export const ContactsItem = styled.div`
 
 export const Schedule = styled.p`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 20px;
+    font-size: 14px;
     font-weight: 400;
     line-height: 1.4;
     letter-spacing: 0.2px;
     color: ${( props ) => props.color || props.theme.colors.white};
-    @media ${device.sm} {
-        font-size: 14px;
+    @media screen and (min-width: 360px) {
+        font-size: 20px;
     }
 `;
 
@@ -123,7 +143,7 @@ export const Email = styled.a`
     display: flex;
     gap: 8px;
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 20px;
+    font-size: 14px;
     font-weight: 400;
     line-height: 1.4;
     letter-spacing: 0.2px;
@@ -135,8 +155,8 @@ export const Email = styled.a`
     &:not(:last-child) {
       margin-bottom: 12px;
     }
-    @media ${device.sm} {
-        font-size: 14px;
+    @media screen and (min-width: 360px) {
+        font-size: 20px;
     }
     &:hover > svg {   
         fill: ${( props ) => props.color || props.theme.colors.white};
@@ -152,10 +172,10 @@ export const Email = styled.a`
 export const SocList = styled.ul`
     display: flex;
     gap: 12px;
-    margin-right: 145px;
-    @media ${device.sm} {
-        margin-right: 0;
-        margin-bottom: 40px;
+    margin-right: 0;
+    margin-bottom: 40px;
+    @media screen and (min-width: 360px) {
+        margin-right: 145px;
     }
     & svg {
         cursor: pointer;
@@ -179,21 +199,22 @@ export const FormBtnWrapper = styled.div`
 `;
 
 export const FormBtn = styled.button`
-    width: 224px;
+    /* width: 320px; */
+    width: 100%;
+    margin: 0 auto;
     border: 4px solid ${( props ) => props.color || props.theme.colors.white};
     border-radius: 16px 0;
     padding: 10px 16px;
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 20px;
-    font-weight: 600;
+    font-size: 16px;
+    font-weight: 500;  
     line-height: 1.4;
     letter-spacing: 0.2px;
     color: ${( props ) => props.color || props.theme.colors.black};
-    @media ${device.sm} {
-        width: 320px;
-        font-size: 16px;
-        font-weight: 500;
-        margin: 0 auto;
+    @media screen and (min-width: 360px) {
+        width: 222px;
+        font-size: 20px;
+        font-weight: 600;
     }
     -webkit-transition: background-color ${transition}, border-color ${transition};
     transition: background-color ${transition}, border-color ${transition}, color ${transition};
@@ -206,26 +227,27 @@ export const FormBtn = styled.button`
 `;
 
 export const TermsRightsWrapper = styled.div`
-    margin-top: 126px;
+    margin-top: 66px;
     text-align: center;
-    @media ${device.sm} {
-        margin-top: 66px;
+    @media screen and (min-width: 360px) {
+        margin-top: 126px;
     }
     & > div {
-        width: 390px;
-        margin: 0 auto 12px auto;
+        width: 100%;
+        margin-top: 66px;
+        margin: 0 auto 9px auto;
         position: relative;
-        @media ${device.sm} {
-            margin-top: 66px;
-            margin: 0 auto 9px auto
+        @media screen and (min-width: 360px) {
+            margin: 0 auto 12px auto;
+            width: 360px;
         }
         & > svg {
             position: absolute;
             top: 50%;
             transform: translateY(-50%);
-            right: 170px;
-            @media ${device.sm} {
-                right: 176px;
+            right: 176px;
+            @media screen and (min-width: 360px) {
+                right: 170px;
         }
         }
     }
@@ -233,32 +255,32 @@ export const TermsRightsWrapper = styled.div`
 
 export const Terms = styled.a`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 16px;
+    font-size: 12px;
     font-weight: 400;
     line-height: 1.4;
-    letter-spacing: 0.16px;
+    letter-spacing: 0.12px;
     color: ${( props ) => props.color || props.theme.colors.white};
-    ${( props ) => props.$first && css`
-        margin-right: 17px; 
-        @media ${device.sm} {
-            margin-right: 15px; 
+    ${( props ) => props.$first && css` 
+        margin-right: 15px;
+        @media screen and (min-width: 360px) {
+            margin-right: 17px;
         }
     `};
-    @media ${device.sm} {
-        font-size: 12px;
-        letter-spacing: 0.12px;
+    @media screen and (min-width: 360px) {
+        font-size: 16px;
+        letter-spacing: 0.16px;
     }
 `;
 
 export const Rights = styled.p`
     font-family: ${( props ) => props.fontFamily || props.theme.fontFamily.primary};
-    font-size: 16px;
+    font-size: 12px;
     font-weight: 300;
     line-height: 1.4;
-    letter-spacing: 0.16px;
+    letter-spacing: 0.12px;
     color: ${( props ) => props.color || props.theme.colors.white}; 
-    @media ${device.sm} {
-        font-size: 12px;
-        letter-spacing: 0.12px;
+    @media screen and (min-width: 360px) {
+        font-size: 16px;
+        letter-spacing: 0.16px;
     }
 `;
