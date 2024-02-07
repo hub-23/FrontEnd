5efa5d1a2ee014aaab89d5399d4f7ef5197c15bf
OIgaB <linkbolya@gmail.com>
2024-02-07 23:55:34 +0200

    add notification with close btn and icon

--- src/components/Footer/QuestionForm/Notification/Notification.jsx
+++ src/components/Footer/QuestionForm/Notification/Notification.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BtnClose } from '../BtnClose/BtnClose';
+import { IconSvg } from '../../../common/IconSvg';
 import * as S from './Notification.styled';
 
 export const Notification = ( { onActiveModal } ) => {
-  console.log( 'ppp' );
   return (
     <S.NotificationWrapper>
+      <BtnClose onActiveModal={ onActiveModal }/>
+      <S.IconWrapper>
+        <IconSvg
+          xlWidth="28px"
+          xlHeight="19px"
+          //   mdWidth="36px"
+          //   mdHeight="36px"
+          //   smWidth="24px"
+          //   smHeight="24px"
+          icon="icon-image-true"
+        />
+      </S.IconWrapper>
       <p>Форму успішно надіслано</p>
       <p>Ваше повідомлення буде оброблено протягом 72 годин та надано фідбек за телефоном або на пошту</p>
     </S.NotificationWrapper>
--- src/components/Footer/QuestionForm/Notification/Notification.styled.js
+++ src/components/Footer/QuestionForm/Notification/Notification.styled.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 import { device } from '../../../../styles/device';
+import { borderGreen } from '../../../../utils/variables.styled';
 
 export const NotificationWrapper = styled.div`
     position: absolute;
-    width: 800px;
+    max-width: 800px;
     height: 382px;
     margin: 0 20px;
     padding: 48px 160px;
     border-radius: 20px 0px;
     background: ${( props ) => props.color || props.theme.colors.white};  
-    background-color: yellowgreen;
 
     @media ${device.md} {  
         max-width: 516px;
@@ -19,49 +19,22 @@ export const NotificationWrapper = styled.div`
     }
 `;
 
-// export const BtnClose = styled.button`
-//     position: absolute;
-//     top: 50px;
-//     right: 40px;
-//     width: 56px;
-//     height: 56px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
+export const IconWrapper = styled.div`
+    width: 67px;
+    height: 67px;
+    border: 1px solid ${borderGreen};
+    border-radius: 50%;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-//     border-width: 1px;
-//     border-style: solid;
-//     border-radius: 20px 0;
-//     border-color: ${( props ) => props.color || props.theme.colors.accent};
-//     background-color: ${( props ) => props.color || props.theme.colors.white};
-//     transition: border-color ${transition}, background-color ${transition};
-//     &:hover,
-//     &:focus,
-//     &:active {
-//         border-color: ${( props ) => props.color || props.theme.colors.accent};
-//         background-color: ${( props ) => props.color || props.theme.colors.accent};
-//         > svg {
-//           stroke: ${( props ) => props.color || props.theme.colors.white};
-//           transition: stroke ${transition};
-//         }
-//     }
-//     > svg {
-//       stroke: ${( props ) => props.color || props.theme.colors.black};
-//       transition: stroke ${transition};
-//     }
 
-//     @media ${device.md} {
-//         top: 16px;
-//         right: 16px;
-//         width: 36px;
-//         height: 36px;
-//         border-radius: 10px 0;
-//     }
-//     @media ${device.sm} {
-//         top: 12px;
-//         right: 12px;
-//         width: 24px;
-//         height: 24px;
-//         border-radius: 8px 0;
-//     }
-// `;
+    @media ${device.md} {  
+        max-width: 516px;
+    }
+    @media ${device.sm} {  
+        max-width: 360px;
+    }
+`;
