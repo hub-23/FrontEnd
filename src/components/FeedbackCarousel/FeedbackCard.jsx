391f38a6fb23cc1784048da6b1983341c39581c3
OIgaB <linkbolya@gmail.com>
2024-02-09 05:17:53 +0200

    resize feedback carousel

--- src/components/FeedbackCarousel/FeedbackCard.jsx
+++ src/components/FeedbackCarousel/FeedbackCard.jsx
@@ -22,7 +22,7 @@ export const FeedbackCard = ( { feedback } ) => {
     setTimeout( checkOverflow, 0 );
 
     const handleResize = () => checkOverflow();
-    window.addEventListener( 'resize', _.debounce( handleResize, 1000 ) );
+    window.addEventListener( 'resize', _.debounce( handleResize, 800 ) );
 
     return () => {
       window.removeEventListener( 'resize', handleResize );
@@ -34,19 +34,23 @@ export const FeedbackCard = ( { feedback } ) => {
       <S.StudentName>{studentName}</S.StudentName>
       <S.Photo src={ photo } alt={ studentName } />
       <Rating rating={ rating } />
-      <S.TeacherNameWrapper>
-        <S.HatIcon width="24px" height="24px">
+      <S.TeacherNameWrapper to='/teacher'>
+        <svg width="24px" height="24px" className='default'>
           <use href={ `${sprite}#icon-hat-graduation` }></use>
-        </S.HatIcon>
+        </svg>
+        <svg width="24px" height="24px" className='active'>
+          <use href={ `${sprite}#icon-hat-graduation-hover` }></use>
+        </svg>
         <S.TeacherName>{teacherName}</S.TeacherName>
       </S.TeacherNameWrapper>
       <S.Feedback ref={ pRef } $overflow={ isOverflowing }>{feedbackText}</S.Feedback>
       {isOverflowing
-        ? <S.CardFooter>
-          <S.Date>{date}</S.Date>
-          <S.Detailed>Детальніше</S.Detailed>
-        </S.CardFooter>
-        : <S.Date>{date}</S.Date>
+        ? (
+          <S.CardFooter>
+            <S.Date>{date}</S.Date>
+            <S.Detailed to='/teacher'>Детальніше</S.Detailed>
+          </S.CardFooter>
+        ) : <S.Date>{date}</S.Date>
       }
     </S.Card>
   );
