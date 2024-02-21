f8bc9eee58ec7f617b43be52d3a289fb98b3c31c
OIgaB <linkbolya@gmail.com>
2024-02-21 19:46:21 +0200

    test

--- src/pages/StudentPage.jsx
+++ src/pages/StudentPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StudentPage = () => {
   return (
     <>
-      <h1 style={ { marginTop: '100px', fontSize: '30px' } }>Student&apos;s Page</h1>
+      <h1 style={ { marginTop: '100px', fontSize: '30px' } }>Student&apos;s Page.</h1>
     </>
   );
 };
