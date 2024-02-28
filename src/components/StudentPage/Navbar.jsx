39452fad709917df3d5ffcfb71dd0527c3079903
L-Marina <marina.liemak@gmail.com>
2024-02-28 23:49:21 +0200

    add styles

--- src/components/StudentPage/Navbar.jsx
+++ src/components/StudentPage/Navbar.jsx
@@ -1,19 +1,58 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import * as S from './Navbar.styled';
+import icon from '../../assets/student/profile-circle.png';
 
 export const Navbar = () => {
 
 	return (
 		<nav>
-			<div>
-				<NavLink to='/student/info'>Особиста інформація</NavLink>
-			</div>
-			<div>
-				<NavLink to='/student/reservation'>Мої бронювання</NavLink>
-			</div>
-			<div>
-				<NavLink to='/student/out'>Вийти</NavLink>
-			</div>
+			<S.Section>
+                <S.Title>
+                    <S.Image src={ icon } alt='icon'></S.Image>
+                    <NavLink to='/student/info'
+                        style={ ( { isActive } ) =>
+                        isActive
+                        ? {
+                            color: '#e3669c',
+                            background: '#f9f9f9',
+                            }
+                        : { color: '#2d2d2d', background:  '#ffffff' }
+                        }>
+                        Особиста інформація
+                    </NavLink>
+                </S.Title>
+			</S.Section>
+			<S.Section>
+                <S.Title>
+				    <NavLink to='/student/reservation'
+                        style={ ( { isActive } ) =>
+                        isActive
+                        ? {
+                            color: '#e3669c',
+                            background: '#f9f9f9',
+                            }
+                        : { color: '#2d2d2d', background:  '#ffffff' }
+                        }>
+                        Мої бронювання
+                    </NavLink>
+                </S.Title>
+			</S.Section>
+			<S.Section>
+                <S.Title>
+				    <NavLink to='/student/out'
+                    style={ ( { isActive } ) =>
+                    isActive
+                    ? {
+                        color: '#e3669c',
+                        background: '#f9f9f9',
+                        }
+                    : { color: '#2d2d2d', background:  '#ffffff' }
+                    }>
+                        Вийти
+                    </NavLink>
+                </S.Title>
+			</S.Section>
 		</nav>
 	)
 }
