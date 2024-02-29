78a3e47859e3a77bd9f6c599483298be17189b4e
L-Marina <marina.liemak@gmail.com>
2024-02-29 10:28:09 +0200

    fix styles

--- src/components/StudentPage/Navbar.jsx
+++ src/components/StudentPage/Navbar.jsx
@@ -1,58 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import * as S from './Navbar.styled';
-import icon from '../../assets/student/profile-circle.png';
 
 export const Navbar = () => {
 
 	return (
-		<nav>
-			<S.Section>
-                <S.Title>
-                    <S.Image src={ icon } alt='icon'></S.Image>
-                    <NavLink to='/student/info'
-                        style={ ( { isActive } ) =>
-                        isActive
-                        ? {
-                            color: '#e3669c',
-                            background: '#f9f9f9',
-                            }
-                        : { color: '#2d2d2d', background:  '#ffffff' }
-                        }>
-                        Особиста інформація
-                    </NavLink>
-                </S.Title>
-			</S.Section>
-			<S.Section>
-                <S.Title>
-				    <NavLink to='/student/reservation'
-                        style={ ( { isActive } ) =>
-                        isActive
-                        ? {
-                            color: '#e3669c',
-                            background: '#f9f9f9',
-                            }
-                        : { color: '#2d2d2d', background:  '#ffffff' }
-                        }>
-                        Мої бронювання
-                    </NavLink>
-                </S.Title>
-			</S.Section>
-			<S.Section>
-                <S.Title>
-				    <NavLink to='/student/out'
-                    style={ ( { isActive } ) =>
-                    isActive
-                    ? {
-                        color: '#e3669c',
-                        background: '#f9f9f9',
-                        }
-                    : { color: '#2d2d2d', background:  '#ffffff' }
-                    }>
-                        Вийти
-                    </NavLink>
-                </S.Title>
-			</S.Section>
-		</nav>
+		<ul>
+			<S.StyledNavLink to='/student/info'>
+                <S.Title>Особиста інформація</S.Title> 
+			</S.StyledNavLink>
+			<S.StyledNavLink to='/student/reservation'>
+                <S.Title> Мої бронювання</S.Title>
+			</S.StyledNavLink>
+			<S.StyledNavLink to='/student/out'>
+                <S.Title>Вийти</S.Title>
+            </S.StyledNavLink>
+		</ul>
 	)
 }
--- src/components/StudentPage/Navbar.styled.js
+++ src/components/StudentPage/Navbar.styled.js
@@ -1,40 +1,30 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Section = styled.section`
+export const StyledNavLink = styled( NavLink )`
+  display: block;
   width: 429px;
   height: 60px;
-  //   border: 1px solid #000000;
-  border-radius: 0px 56px 56px 0px;
+  color: #2d2d2d;
 
-  &:hover {
-  }
-
-  &:active {
+  &.active {
+    border-radius: 0px 56px 56px 0px;
     background-color: #f9f9f9;
     color: #e3669c;
   }
 `;
 
-export const Image = styled.image`
-  width: 24px;
-  height: 24px;
-`;
+// export const Image = styled.img`
+//     width: 24px;
+//     height: 24px;
+// `;
 
 export const Title = styled.h2`
-  //   width: 245px;
+  width: 245px;
   height: 28px;
   padding: 16px 0px 16px 160px;
   white-space: nowrap;
   font-size: 20px;
   font-weight: 400;
   line-height: 140%;
-  color: #e3669c;
-
-  &:hover {
-    color: #e3669c;
-  }
-
-  &:active {
-    color: #e3669c;
-  }
 `;
