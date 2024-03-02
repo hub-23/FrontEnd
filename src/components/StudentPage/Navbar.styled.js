c8a2580fbdf8937698ec81be930e1d2ee5626278
OIgaB <linkbolya@gmail.com>
2024-03-02 21:54:53 +0200

    add inputs, refactor formik form and restyle PersonalInfo page

--- src/components/StudentPage/Navbar.styled.js
+++ src/components/StudentPage/Navbar.styled.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  /* width: 100%; */
   min-height: 100vw;
 `;
 
