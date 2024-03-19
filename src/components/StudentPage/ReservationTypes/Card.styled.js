534ece277f698dedd25af29466d5352a4cdf948c
OIgaB <linkbolya@gmail.com>
2024-03-19 02:13:46 +0200

    add registration cards layout

---
+++ src/components/StudentPage/ReservationTypes/Card.styled.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { Button } from '../../common/button/Button';
+import { grayStroke, grayText } from '../../../utils/variables.styled';
+
+export const Container = styled.li`
+    width: 740px;
+    height: 268px;
+    border: 1px solid ${props => props.color || props.theme.colors.black};
+    border-radius: 0px 20px 0px 20px;
+    border-left: 8px solid ${props => props.color || props.theme.colors.primary};
+    padding: 32px;
+    display: flex;
+`;
+
+export const GeneralInfo = styled.div`
+    padding-right: 32px;
+    border-right: 1px solid ${props => props.color || grayStroke};
+
+    .personal-data {
+        display: flex;
+        align-items: center;
+        margin-bottom: 76px;
+
+        img {
+            width: 80px;
+            height: 80px;
+            border-radius: 0 8px 0 8px;
+            margin-right: 16px;
+        }
+
+        div {
+            span {
+                display: flex;
+                gap: 6px;
+
+                .name {
+                    font-family: ${props => props.fontFamily || props.theme.fontFamily.primary};
+                    font-weight: 600;
+                    font-size: 20px;
+                    line-height: calc(28 / 20);
+                }
+            }
+            .phone {
+                font-family: ${props => props.fontFamily || props.theme.fontFamily.primary};
+                font-weight: 400;
+                font-size: 20px;
+                line-height: calc(28 / 20);
+            }
+        }
+    }
+`;
+
+export const TimeTable = styled.div`
+    padding-left: 32px;
+    div:not(:last-child) {
+        margin-bottom: 16px;
+    }
+`;
+
+export const FeedbackButton = styled( Button )`
+    width: 298px;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1;
+`;
+
+export const Static = styled.span`
+    display: inline-block;
+    width: 86px;
+    margin-right: 16px;
+    font-family: ${props => props.fontFamily || props.theme.fontFamily.primary};
+    font-size: 20px;
+    line-height: calc(28 / 20);
+    color: ${props => props.color || grayText};
+`;
+
+export const Dynamic = styled.span`
+    font-family: ${props => props.fontFamily || props.theme.fontFamily.primary};
+    font-size: 20px;
+    line-height: calc(28 / 20);
+    color: ${props => props.color || props.theme.colors.black};
+`;
---
+++ src/components/StudentPage/ReservationTypes/NotActiveReservations.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import * as S from './NotActiveReservations.styled';
+
+export const NotActiveReservations = () => {
+    return (
+        <div style={ { backgroundColor: 'yellow' } }>
+            Неактивні
+        </div>
+    );
+};
