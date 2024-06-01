7242e0d1f523527d29de06750853a6c8d58ef9aa
YuriiLeo <yuriileontenko@gmail.com>
2024-06-01 17:42:56 +0200

    feat: add delete profile logic

--- src/components/StudentPage/modals/DeleteProfile.jsx
+++ src/components/StudentPage/modals/DeleteProfile.jsx
@@ -7,116 +7,115 @@ import { Input } from '../../common/modalElements/Input';
 import sprite from '../../../assets/sprite.svg';
 import * as S from './DeleteProfile.styled';
 
+export const DeleteProfile = ( {
+  onDeleteProfileModalClose,
+  onNotificationShow,
+} ) => {
+  const { user } = useAuth();
 
-export const DeleteProfile = ( { onDeleteProfileModalClose, onNotificationShow } ) => {
-    const { user } = useAuth();
+  const reasonsToDelete = [
+    'Незадоволеність послугами',
+    'Надмірний спам',
+    'Зміни в умовах обслуговування',
+    'Особисті причини',
+    'Технічні проблеми',
+    'Інше',
+  ];
 
-    const reasonsToDelete = [
-        'Незадоволеність послугами',
-        'Надмірний спам',
-        'Зміни в умовах обслуговування',
-        'Особисті причини',
-        'Технічні проблеми',
-        'Інше'
-    ];
-    
-    const customErrorMessage = 'Оберіть причину видалення профілю';
+  const customErrorMessage = 'Оберіть причину видалення профілю';
 
-    const schema = object( {
-        reason: string()
-          .test( 'is-valid-reason', customErrorMessage, value => {
-            return reasonsToDelete.includes( value );
-          } )
-          .required( 'Вкажіть тему повідомлення' ),
-        accept: boolean()
-          .oneOf( [ true ], 'Підтвердіть, що ви згодні з умовами видалення' )
-          .required( 'Підтвердіть, що ви згодні з умовами видалення' ),
-    } );
-    
-    const initialValues = {
-        reason: localStorage.getItem( 'delete-profile-reason' ) || '',
-        accept: false,
-    };
+  const schema = object( {
+    reason: string()
+      .test( 'is-valid-reason', customErrorMessage, value => {
+        return reasonsToDelete.includes( value );
+      } )
+      .required( 'Вкажіть тему повідомлення' ),
+    accept: boolean()
+      .oneOf( [ true ], 'Підтвердіть, що ви згодні з умовами видалення' )
+      .required( 'Підтвердіть, що ви згодні з умовами видалення' ),
+  } );
 
-    const handleSubmit = ( values, { resetForm } ) => {
-        console.log( 'submit - values:', { email: user.email, ...values } ); // відправка на бек
+  const initialValues = {
+    reason: localStorage.getItem( 'delete-profile-reason' ) || '',
+    accept: false,
+  };
 
-        localStorage.removeItem( 'delete-profile-reason' );    
-        resetForm();
-        onDeleteProfileModalClose();
-        onNotificationShow();
-    };
+  const handleAccountDelete = ( values, { resetForm } ) => {
+    console.log( 'submit - values:', { email: user.email, ...values } ); // відправка на бек
 
-    return (
-      <S.Container>
-        <BtnClose onActiveModal={ onDeleteProfileModalClose } />
-        <S.Title>Видалення профілю</S.Title>
-        <Formik
-            initialValues={ initialValues }
-            validationSchema={ schema }
-            onSubmit={ handleSubmit }
-        >
-          {formik => {
-            const {
-                errors: { reason },
-                touched,
-                values,
-            } = formik;
+    localStorage.removeItem( 'delete-profile-reason' );
+    resetForm();
+    onDeleteProfileModalClose();
+    onNotificationShow();
+  };
 
-            const errReason = reason && touched.reason;
+  return (
+    <S.Container>
+      <BtnClose onActiveModal={ onDeleteProfileModalClose } />
+      <S.Title>Видалення профілю</S.Title>
+      <Formik
+        initialValues={ initialValues }
+        validationSchema={ schema }
+        onSubmit={ handleAccountDelete }
+      >
+        {formik => {
+          const {
+            errors: { reason },
+            touched,
+            values,
+          } = formik;
 
-            return (
-                <S.FormField autoComplete="off">
-                    <div>
-                        <Input
-                            type="text"
-                            name="reason"
-                            placeholder="Причина видалення"
-                            error={ errReason }
-                            value={ values.reason }
-                            dropdown
-                            data={ reasonsToDelete }
-                            formik={ formik }
-                            $topic
-                            $left='12px'
-                            readOnly
-                            component="delete-profile"
-                        />
-                        <S.CheckboxLabel>
-                            <div>
-                                <Field
-                                    type="checkbox"
-                                    name="accept"
-                                />
-                                <svg aria-label="mark" width="20px" height="20px">
-                                    <use href={ `${sprite}#icon-check-mark` }></use>
-                                </svg>
-                            </div>
-                            <div>
-                                <p>
-                                    Видаляючи свій обліковий запис, я розумію,
-                                    що втрачу доступ до контактних данних вчителів,
-                                    можливості писати коментарі та інших розширених функцій сайту     
-                                </p>
-                                <ErrorMessage name="accept" component="span" />
-                            </div>
-                        </S.CheckboxLabel>                        
-                    </div>
-                    <div>
-                        <S.DeleteBtn type='submit' variant="blue">
-                            Видалити профіль
-                        </S.DeleteBtn>
-                        <S.CancelBtn
-                            variant="blueGradientedBorder"
-                            onClick={ onDeleteProfileModalClose }
-                        >
-                            Скасувати
-                        </S.CancelBtn>                        
-                    </div>
-                </S.FormField>
-            );
-          }}
-        </Formik>
-      </S.Container>
-    )
+          const errReason = reason && touched.reason;
+
+          return (
+            <S.FormField autoComplete="off">
+              <div>
+                <Input
+                  type="text"
+                  name="reason"
+                  placeholder="Причина видалення"
+                  error={ errReason }
+                  value={ values.reason }
+                  dropdown
+                  data={ reasonsToDelete }
+                  formik={ formik }
+                  $topic
+                  $left="12px"
+                  readOnly
+                  component="delete-profile"
+                />
+                <S.CheckboxLabel>
+                  <div>
+                    <Field type="checkbox" name="accept" />
+                    <svg aria-label="mark" width="20px" height="20px">
+                      <use href={ `${sprite}#icon-check-mark` }></use>
+                    </svg>
+                  </div>
+                  <div>
+                    <p>
+                      Видаляючи свій обліковий запис, я розумію, що втрачу
+                      доступ до контактних данних вчителів, можливості писати
+                      коментарі та інших розширених функцій сайту
+                    </p>
+                    <ErrorMessage name="accept" component="span" />
+                  </div>
+                </S.CheckboxLabel>
+              </div>
+              <div>
+                <S.DeleteBtn type="submit" variant="blue">
+                  Видалити профіль
+                </S.DeleteBtn>
+                <S.CancelBtn
+                  variant="blueGradientedBorder"
+                  onClick={ onDeleteProfileModalClose }
+                >
+                  Скасувати
+                </S.CancelBtn>
+              </div>
+            </S.FormField>
+          );
+        }}
+      </Formik>
+    </S.Container>
+  );
 };
