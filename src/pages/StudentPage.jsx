5a4fedfb40ad9bff7807d03278d7f5c9aa7a684e
YuriiLeo <yuriileontenko@gmail.com>
2024-06-01 17:39:06 +0200

    style: added responsiveness

--- src/pages/StudentPage.jsx
+++ src/pages/StudentPage.jsx
@@ -2,29 +2,38 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from 'components/StudentPage/Navbar';
 import styled from 'styled-components';
+import { device } from 'utils/device';
 
 const Container = styled.section`
-    display: flex;
-    width: 100%;
-    min-height: 100vw;
-    margin-top: 100px;
-    font-size: 30px;
-    overflow: hidden;
+  display: flex;
+  width: 100%;
+  min-height: 100vw;
+  margin-top: 128px;
+  font-size: 30px;
+  overflow: hidden;
+
+  @media ${device.md} {
+    display: block;
+  }
+
+  @media ${device.sm} {
+    margin-top: 80px;
+  }
 `;
 
 const Content = styled.section`
-    width: 100%;
+  width: 100%;
 `;
 
 const StudentPage = () => {
   return (
     <>
-		<Container>
-			<Navbar />
-			<Content>
-				<Outlet />
-			</Content>
-		</Container>
+      <Container>
+        <Navbar />
+        <Content>
+          <Outlet />
+        </Content>
+      </Container>
     </>
   );
 };
