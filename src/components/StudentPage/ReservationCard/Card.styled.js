81bca2bf864c1d5d3f2a7e9648d1bf6964698f22
Serhii Svitlychnyi <svetlichniy1905@gmail.com>
2024-10-02 13:31:43 +0300

    made an adaptive version for the student reservation page

--- src/components/StudentPage/ReservationCard/Card.styled.js
+++ src/components/StudentPage/ReservationCard/Card.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { Button } from '../../common/button/Button';
 import { grayStroke, grayText } from '../../../utils/variables.styled';
+import { device } from 'utils/device';
 
 export const Container = styled.li`
     display: flex;
@@ -11,22 +12,47 @@ export const Container = styled.li`
     border-left: 8px solid ${ ( { theme, $status  } ) =>
         $status === 'active' ? theme.colors.accent : theme.colors.primary };
     border-radius: 0px 20px 0px 20px;
+
+    @media ${device.md} {
+      width: auto;
+      height: auto;
+      padding: 24px 28px;
+      flex-direction:column;
+    }
 `;
 
 export const GeneralInfo = styled.div`
     padding-right: 32px;
     border-right: 1px solid ${props => props.color || grayStroke};
 
+    @media ${device.md} {
+      border:none;
+      border-bottom: 1px solid ${props => props.color || grayStroke};
+      padding-bottom: 20px;
+      padding-right: 0px;
+    }
+
     .personal-data {
         display: flex;
         align-items: center;
         margin-bottom: 76px;
+
+      @media ${device.md} {
+        margin-bottom: 0px;
+      }
 
         img {
             width: 80px;
             height: 80px;
             border-radius: 0 8px 0 8px;
             margin-right: 16px;
+
+          @media ${device.md} {
+                width: 64px;
+                height: 64px;
+                border-radius: 0 8px 0 8px;
+                margin-right: 16px;
+          }
         }
 
         div {
@@ -39,6 +65,10 @@ export const GeneralInfo = styled.div`
                     font-weight: 600;
                     font-size: 20px;
                     line-height: calc(28 / 20);
+
+      @media ${device.md} {
+        font-size: 16px;
+      }
                 }
             }
             .phone {
@@ -46,6 +76,10 @@ export const GeneralInfo = styled.div`
                 font-weight: 400;
                 font-size: 20px;
                 line-height: calc(28 / 20);
+
+      @media ${device.md} {
+        font-size: 16px;
+      }
             }
         }
     }
@@ -56,6 +90,12 @@ export const TimeTable = styled.div`
     div:not(:last-child) {
         margin-bottom: 16px;
     }
+
+    @media ${device.md} {
+      padding: 12px 0;
+      border-bottom: 1px solid ${props => props.color || grayStroke};
+      margin-bottom:20px;
+    }
 `;
 
 export const FeedbackButton = styled( Button )`
@@ -63,6 +103,26 @@ export const FeedbackButton = styled( Button )`
     font-size: 20px;
     font-weight: 600;
     line-height: 1;
+
+    @media ${device.md} {
+      display:none;
+    }
+`;
+
+export const FeedbackButtonMobile = styled( Button )`
+    display:none;
+    width: 384px;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.4;
+
+    @media ${device.md} {
+      display: block;
+    }
+
+    @media ${device.sm} {
+      width: 244px;
+    }
 `;
 
 export const Static = styled.span`
@@ -73,6 +133,10 @@ export const Static = styled.span`
     font-size: 20px;
     line-height: calc(28 / 20);
     color: ${props => props.color || grayText};
+
+@media ${device.md} {
+        font-size: 16px;
+      }
 `;
 
 export const Dynamic = styled.span`
@@ -80,4 +144,8 @@ export const Dynamic = styled.span`
     font-size: 20px;
     line-height: calc(28 / 20);
     color: ${props => props.color || props.theme.colors.black};
+
+@media ${device.md} {
+        font-size: 16px;
+      }
 `;
--- src/components/common/privatePage/ItemNavbar.styled.js
+++ src/components/common/privatePage/ItemNavbar.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import { accent, black, lightGrey, transition } from 'utils/variables.styled';
+import { accent, black, lightGrey, primary, transition } from 'utils/variables.styled';
+import { device } from 'utils/device';
 
 export const Link = styled( NavLink )`
   display: flex;
@@ -8,6 +9,7 @@ export const Link = styled( NavLink )`
   align-items: center;
   padding: ${( { $padding } ) => $padding || '16px 24px 16px 160px'};
 
+  border: 1px solid transparent;
   border-radius: ${( { $borderRadius } ) => $borderRadius || '0 56px 56px 0'};
   color: ${black};
   transition: background-color ${transition}, color ${transition};
@@ -16,9 +18,44 @@ export const Link = styled( NavLink )`
     color: ${accent};
     background-color: ${lightGrey};
   }
+
+  svg {
+    @media ${device.md} {
+      display: none;
+    }
+  }
+
+  @media ${device.xl} {
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-bottom: 1px;
+    border: none;
+    border-bottom: 1px solid transparent;
+    border-radius: 0;
+    transition: border ${transition}, color ${transition};
+
+    &.active {
+      color: ${primary};
+      background-color: transparent;
+    }
+
+    &:hover{
+      color: ${primary};
+      border-color: ${primary};
+    }
+  }
+
+  @media ${device.md} {
+    padding: 0px 0px 1px;
+  }
 `;
 
 export const Text = styled.h3`
   font-size: 20px;
-  line-height: calc(28 / 20);
+  line-height: calc( 28 / 20 );
+
+  @media ${device.md} {
+    font-size: 16px;
+    line-height: calc( 28 / 16 );
+  }
 `;
